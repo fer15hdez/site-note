63a4d34dddde25e2c640a06a0419f29130cc93cd
Fernando Velazquez <fernan.hdez2015@gmail.com>
2026-06-02 17:43:36 -0300

    more

--- public/posts/java/Spring Boot/cursoSpringBoot/src/main/java/cursoSpringBoot/exceptions/DeleteEntityNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package cursoSpringBoot.exceptions;
-
-public class DeleteEntityNotFoundException extends RuntimeException{
-    public DeleteEntityNotFoundException(String message) {
-        super(message);
-    }
-}
